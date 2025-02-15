eca0ed43f717f9a374662bc3c87ddcf4ea2ac3dd
jeanmfdias <jeanmfdias@gmail.com>
2025-02-15 19:04:00 -0300

    Add class to handle errors

---
+++ src/main/java/com/managementpatients/api/infra/HandleErrors.java
@@ -0,0 +1,31 @@
+package com.managementpatients.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandleErrors {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors()
+                .stream()
+                .map(DataValidationError::new)
+                .toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private record DataValidationError(String field, String message) {
+        public DataValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
