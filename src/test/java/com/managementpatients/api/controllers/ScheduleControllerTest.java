1baca600c5f28d7a500a44347c7de7b5e3c8bf8b
jeanmfdias <jeanmfdias@gmail.com>
2025-03-11 14:22:45 -0300

    feat: add test to schedule controller

--- src/test/java/com/managementpatients/api/controllers/ScheduleControllerTest.java
+++ src/test/java/com/managementpatients/api/controllers/ScheduleControllerTest.java
@@ -1,13 +1,24 @@
 package com.managementpatients.api.controllers;
 
+import com.managementpatients.api.domains.doctor.Specialty;
+import com.managementpatients.api.domains.schedule.Schedule;
+import com.managementpatients.api.domains.schedule.dto.CreateDataScheduleDto;
+import com.managementpatients.api.domains.schedule.dto.ScheduleDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,10 +26,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ScheduleControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<CreateDataScheduleDto> createDataScheduleDtoJson;
+
+    @Autowired
+    private JacksonTester<ScheduleDto> scheduleDtoJson;
 
     @Test
     @DisplayName("Should 400 status code when information wrong")
@@ -29,6 +47,30 @@ class ScheduleControllerTest {
                 .getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Should 200 status code when information right")
+    @WithMockUser
+    void testShouldCreateResultSuccess200() throws Exception {
+        var date = LocalDateTime.now().plusHours(1);
+
+        var response = mvc.perform(
+                    post("/schedule")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(createDataScheduleDtoJson.write(
+                                    new CreateDataScheduleDto(21L, 51L, date, Specialty.CARDIOLOGY)
+                            ).getJson())
+                )
+                .andReturn()
+                .getResponse();
+
+//        var expectJson = scheduleDtoJson.write(
+//                new ScheduleDto(null, 21L, 51L, date)
+//        ).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo(expectJson);
     }
 
 }
